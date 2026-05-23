ccec2adb49381af42113fe14bb9bc66b5cfcd8c0
Fritz Agildere <fritz.agildere@gmail.com>
2026-05-23 18:45:31 +0200

    danger

--- MASS/Spectroscopy/plot.py
+++ MASS/Spectroscopy/plot.py
@@ -9,11 +9,13 @@ import matplotlib.pyplot as plt
 
 ### continua ###
 
-comaE, comaF, comaF = np.genfromtxt('chandra/acisf13996N003_evt2.txt', unpack=True)
-persE, persF, persF = np.genfromtxt('chandra/acisf04952N004_evt2.txt', unpack=True)
+comaE, comaF, comaFerr = np.genfromtxt('catalog/Chandra/acisf13996N003_evt2.txt', unpack=True)
+persE, persF, persFerr = np.genfromtxt('catalog/Chandra/acisf04952N004_evt2.txt', unpack=True)
+testE, testF, testFerr = np.genfromtxt('catalog/Chandra/acisf15173N003_evt2.txt', unpack=True)
 
 plt.plot(persE, persF, lw=0.85, label='Perseus')
 plt.plot(comaE, comaF, lw=0.85, label='Coma')
+plt.plot(testE, testF, lw=0.85, label='A85')
 
 plt.xlabel(r'$E \mathbin{/} \unit{\kilo\electronvolt}$')
 plt.ylabel(r'$N \mathbin{/} \unit{\per\second\per\kilo\electronvolt}$', labelpad=-10)
