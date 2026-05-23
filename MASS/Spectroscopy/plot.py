b43e59120d2c950cd655569787bd01470591dab0
Fritz Agildere <fritz.agildere@gmail.com>
2026-05-23 04:35:40 +0200

    idekanymore

--- MASS/Spectroscopy/plot.py
+++ MASS/Spectroscopy/plot.py
@@ -1,3 +1,5 @@
+### preamble ###
+
 import scipy as sci
 import pandas as pd
 import numpy as np
@@ -5,4 +7,21 @@ import matplotlib as mpl
 
 import matplotlib.pyplot as plt
 
+### continua ###
 
+comaE, comaF, comaF = np.genfromtxt('chandra/acisf13996N003_evt2.txt', unpack=True)
+persE, persF, persF = np.genfromtxt('chandra/acisf04952N004_evt2.txt', unpack=True)
+
+plt.plot(persE, persF, lw=0.85, label='Perseus')
+plt.plot(comaE, comaF, lw=0.85, label='Coma')
+
+plt.xlabel(r'$E \mathbin{/} \unit{\kilo\electronvolt}$')
+plt.ylabel(r'$N \mathbin{/} \unit{\per\second\per\kilo\electronvolt}$', labelpad=-10)
+
+plt.yscale('log')
+
+#plt.gca().yaxis.set_major_formatter(mpl.ticker.FormatStrFormatter('%g'))
+
+plt.legend()
+
+plt.savefig('content/continua.pdf')
