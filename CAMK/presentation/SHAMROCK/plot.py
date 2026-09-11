3939d59c4b834eed716ce2977866f6147854b6a5
Fritz Agildere <fritz.agildere@gmail.com>
2026-09-11 02:10:50 +0200

    plots

--- CAMK/presentation/SHAMROCK/plot.py
+++ CAMK/presentation/SHAMROCK/plot.py
@@ -4,8 +4,43 @@ plot_density('../../PLUTO/Simulation_Main/BH_VISC_HD/', time=0.0)
 plot_density('../../PLUTO/Simulation_Main/BH_VISC_HD/', time=250.0)
 plot_density('../../PLUTO/Simulation_Main/BH_VISC_HD/', time=500.0)
 plot_density('../../PLUTO/Simulation_Main/BH_VISC_HD/', time=750.0)
+
 plot_density('../../PLUTO/Simulation_Main/BH_VISC_HD_TRC/', time=0.0)
 plot_density('../../PLUTO/Simulation_Main/BH_VISC_HD_TRC/', time=250.0)
 plot_density('../../PLUTO/Simulation_Main/BH_VISC_HD_TRC/', time=500.0)
 plot_density('../../PLUTO/Simulation_Main/BH_VISC_HD_TRC/', time=750.0)
+
+plot_tracer('../../PLUTO/Simulation_Main/BH_VISC_HD/', time=0.0, mode='truth')
+plot_tracer('../../PLUTO/Simulation_Main/BH_VISC_HD/', time=250.0, mode='truth')
+plot_tracer('../../PLUTO/Simulation_Main/BH_VISC_HD/', time=500.0, mode='truth')
+plot_tracer('../../PLUTO/Simulation_Main/BH_VISC_HD/', time=750.0, mode='truth')
+
 plot_tracer('../../PLUTO/Simulation_Main/BH_VISC_HD_TRC/', time=0.0, mode='truth')
+plot_tracer('../../PLUTO/Simulation_Main/BH_VISC_HD_TRC/', time=250.0, mode='truth')
+plot_tracer('../../PLUTO/Simulation_Main/BH_VISC_HD_TRC/', time=500.0, mode='truth')
+plot_tracer('../../PLUTO/Simulation_Main/BH_VISC_HD_TRC/', time=750.0, mode='truth')
+
+plot_tracer('../../PLUTO/Simulation_Main/BH_VISC_HD_TRC/', time=0.0, mode='recover')
+plot_tracer('../../PLUTO/Simulation_Main/BH_VISC_HD_TRC/', time=250.0, mode='recover')
+plot_tracer('../../PLUTO/Simulation_Main/BH_VISC_HD_TRC/', time=500.0, mode='recover')
+plot_tracer('../../PLUTO/Simulation_Main/BH_VISC_HD_TRC/', time=750.0, mode='recover')
+
+plot_tracer('../../PLUTO/Simulation_Main/BH_VISC_HD_TRC/', time=0.0, compare=True)
+plot_tracer('../../PLUTO/Simulation_Main/BH_VISC_HD_TRC/', time=250.0, compare=True)
+plot_tracer('../../PLUTO/Simulation_Main/BH_VISC_HD_TRC/', time=500.0, compare=True)
+plot_tracer('../../PLUTO/Simulation_Main/BH_VISC_HD_TRC/', time=750.0, compare=True)
+
+plot_tracer('../../PLUTO/Simulation_Main/BH_VISC_RAD_HD_TRC/', time=0.0, mode='truth')
+plot_tracer('../../PLUTO/Simulation_Main/BH_VISC_RAD_HD_TRC/', time=250.0, mode='truth')
+plot_tracer('../../PLUTO/Simulation_Main/BH_VISC_RAD_HD_TRC/', time=500.0, mode='truth')
+plot_tracer('../../PLUTO/Simulation_Main/BH_VISC_RAD_HD_TRC/', time=750.0, mode='truth')
+
+plot_tracer('../../PLUTO/Simulation_Main/BH_VISC_RAD_HD_TRC/', time=0.0, mode='recover')
+plot_tracer('../../PLUTO/Simulation_Main/BH_VISC_RAD_HD_TRC/', time=250.0, mode='recover')
+plot_tracer('../../PLUTO/Simulation_Main/BH_VISC_RAD_HD_TRC/', time=500.0, mode='recover')
+plot_tracer('../../PLUTO/Simulation_Main/BH_VISC_RAD_HD_TRC/', time=750.0, mode='recover')
+
+plot_tracer('../../PLUTO/Simulation_Main/BH_VISC_RAD_HD_TRC/', time=0.0, compare=True)
+plot_tracer('../../PLUTO/Simulation_Main/BH_VISC_RAD_HD_TRC/', time=250.0, compare=True)
+plot_tracer('../../PLUTO/Simulation_Main/BH_VISC_RAD_HD_TRC/', time=500.0, compare=True)
+plot_tracer('../../PLUTO/Simulation_Main/BH_VISC_RAD_HD_TRC/', time=750.0, compare=True)
